b0e31ebfc78d1dfb35860fa64eed68cbb0ece4af
Geoffroy <geoffroy.oudoumanessah@gmail.com>
2022-05-05 17:03:07 +0200

    add polyak option, burnin option

--- GenStudentMixtures/GenStudentMixtures.py
+++ GenStudentMixtures/GenStudentMixtures.py
@@ -3,9 +3,6 @@ import numpy as np
 from scipy.special import digamma
 from scipy.optimize import brentq
 
-import multiprocessing
-from joblib import Parallel, delayed
-
 from itertools import permutations
 
 from numba import jit
@@ -22,7 +19,8 @@ from pymanopt.manifolds import Stiefel
 
 
 class GenStudentMixtures:
-    def __init__(self, pi, mu, A, D, nu, solver, max_iterations=None, verbose=False):
+    def __init__(self, pi, mu, A, D, nu, solver, burnin=False, polyak=None, polyak2=None, max_iterations=None,
+                 verbose=False):
         # TODO put the statistics as parameter to be able to store the model before loading new data
         # TODO integrate initialization if pi, mu A, D, nu are None
         self.pi = pi
@@ -37,7 +35,10 @@ class GenStudentMixtures:
         self.D_hist = []
         self.nu_hist = []
 
-        self.stat_hist = []
+        self.burnin = burnin
+
+        self.polyak = polyak
+        self.polyak2 = polyak2
 
         self.solver = solver
 
@@ -103,11 +104,13 @@ class GenStudentMixtures:
     # Update D
     @staticmethod
     def _loss(D, matQuadk):
-        tmp = np.swapaxes(D, -2, -1) @ matQuadk
-        tmp = tmp @ D
-        tmp = np.diagonal(tmp, 0, -2, -1)
-        quadForm = np.diagonal(tmp, 0, -2, -1)
-        return np.sum(quadForm)
+        loss = 0
+        M = len(D)
+        E = np.eye(M)
+        for m in range(M):
+            quadForm = D @ E[:, m].T @ matQuadk[m] @ D @ E[:, m]
+            loss += np.log(quadForm)
+        return loss
 
     @staticmethod
     def _compute_matQuad(s1, S2, s3):
@@ -115,7 +118,6 @@ class GenStudentMixtures:
         return S2 - np.expand_dims(tmp, -1) @ s1[:, :, None, :]
 
     def _best_permutation(self, D, matQuad):
-        # TODO to optimize
         D_opt = np.zeros(D.shape)
         for k in range(len(D)):
             minim_permuted = np.inf
@@ -129,37 +131,83 @@ class GenStudentMixtures:
         return D_opt
 
     def _update_D(self, s1, S2, s3):
+
         def find_cost(matQuadk, manifold):
             @pymanopt.function.numpy(manifold)
             def cost(D):
-                tmp = np.swapaxes(D, -2, -1) @ matQuadk
-                tmp = tmp @ D
-                tmp = np.diagonal(tmp, 0, -2, -1)
-                quadForm = np.diagonal(tmp, 0, -2, -1)
-                return np.sum(quadForm)
+                loss = 0
+                M = len(D)
+                E = np.eye(M)
+                for m in range(M):
+                    quadForm = D @ E[:, m].T @ matQuadk[m] @ D @ E[:, m]
+                    loss += np.log(quadForm)
+                return loss
 
             @pymanopt.function.numpy(manifold)
             def grad(D):
-                # TODO try to avoid the loop even if M is small
                 grad_value = np.zeros(D.shape)
                 M = len(D)
+                E = np.eye(M)
                 for m in range(M):
-                    grad_value[:, m] = 2 * matQuadk[m] @ D[:, m]
+                    quadForm = D @ E[:, m].T @ matQuadk[m] @ D @ E[:, m]
+                    grad_value[:, m] = 2 * matQuadk[m] @ D @ E[:, m] / quadForm
                 return grad_value
 
             return cost, grad
 
+        """
+        def find_cost(matQuadk, manifold):
+            @pymanopt.function.autograd(manifold)
+            def cost(D):
+                loss = 0
+                M = len(D)
+                E = np.eye(M)
+                for m in range(M):
+                    quadForm = D @ E[:, m].T @ matQuadk[m] @ D @ E[:, m]
+                    loss += autonp.log(quadForm)
+                return loss
+            return cost
+        """
+
         def opti_D(matQuadk, D_init):
-            manifold = Stiefel(*self.D[0].shape)
+            manifold = Stiefel(*D_init.shape)
             cost, grad = find_cost(matQuadk, manifold)
             problem = pymanopt.Problem(manifold, cost, egrad=grad)
+            # D_init_sa = self._SA_init(D_init, matQuadk)
+
             return self.solver.solve(problem, D_init)
 
         matQuad = self._compute_matQuad(s1, S2, s3)
-        d = (delayed(opti_D)(matQuad[k], self.D[k].copy()) for k in range(len(s1)))
-        D_tmp = np.array(Parallel(n_jobs=multiprocessing.cpu_count())(d))
+
+        D_tmp, D_tmp_log = np.zeros(self.D.shape), np.zeros(self.D.shape)
+        K, M, _ = self.D.shape
+        for k in range(K):
+            D_tmp[k] = opti_D(matQuad[k], self.D[k].copy())
 
         return self._best_permutation(D_tmp, matQuad)
+
+    def _SA_init(self, D_initk, matQuadk, times=100, T_0=20):
+        M, _ = D_initk.shape
+        old_val = self._loss(D_initk, matQuadk)
+        for t in range(times):
+            Q = np.eye(M)
+            i = np.random.randint(M - 1)
+            j = np.random.randint(i + 1, M)
+            angle = np.random.uniform(-2 * np.pi, 2 * np.pi)
+            Q[i, i], Q[j, j] = np.cos(angle), np.cos(angle)
+            Q[i, j] = np.sin(angle)
+            Q[j, i] = - np.sin(angle)
+            new_D = Q @ D_initk
+            new_val = self._loss(new_D, matQuadk)
+
+            r = np.random.rand()
+            diff = new_val - old_val
+            temp = T_0 / (1 + t ** 2)
+            if np.exp(-diff / temp) > r:
+                old_val = new_val
+                D_initk = new_D.copy()
+
+        return D_initk
 
     # Update nu
     @staticmethod
@@ -176,18 +224,43 @@ class GenStudentMixtures:
                 new_nu[k, m] = brentq(fun, .01, 100)
         return new_nu.astype(np.float64)
 
-    def _updateParams(self, stat):
+    def _updateParams(self, stat, i):
         s0 = stat['s0']
         s1 = stat['s1'] / s0[:, None, None]
         S2 = stat['S2'] / s0[:, None, None, None]
         s3 = stat['s3'] / np.expand_dims(s0, -1)
         s4 = stat['s4'] / np.expand_dims(s0, -1)
-
-        self.pi = self._update_pi(s0)
-        self.D = self._update_D(s1, S2, s3)
-        self.mu, v = self._update_mu(s1, s3)
-        self.A = self._update_A(v, S2, s3)
-        self.nu = self._update_nu(s3, s4)
+        if self.polyak is None and self.polyak2 is None:
+            self.pi = self._update_pi(s0)
+            self.D = self._update_D(s1, S2, s3)
+            self.mu, v = self._update_mu(s1, s3)
+            self.A = self._update_A(v, S2, s3)
+            self.nu = self._update_nu(s3, s4)
+        else:
+            if self.polyak is not None:
+                if i >= self.polyak:
+                    norm1, norm2 = 1 / (i - self.polyak + 1), i - self.polyak
+                    self.pi = norm1 * (norm2 * self.pi + self._update_pi(s0))
+                    self.D = norm1 * (norm2 * self.D + self._update_D(s1, S2, s3))
+                    mu_tmp, v = self._update_mu(s1, s3)
+                    self.mu = norm1 * (norm2 * self.mu + mu_tmp)
+                    self.A = norm1 * (norm2 * self.A + self._update_A(v, S2, s3))
+                    self.nu = norm1 * (norm2 * self.nu + self._update_nu(s3, s4))
+            else:
+                polyak1, polyak2 = self.polyak2
+                if i >= polyak1:
+                    norm1, norm2 = 1 / (i - polyak1 + 1), i - polyak1
+                    self.pi = norm1 * (norm2 * self.pi + self._update_pi(s0))
+                    self.D = norm1 * (norm2 * self.D + self._update_D(s1, S2, s3))
+                    mu_tmp, v = self._update_mu(s1, s3)
+                    self.mu = norm1 * (norm2 * self.mu + mu_tmp)
+                    if i < polyak2:
+                        self.A = self._update_A(v, S2, s3)
+                        self.nu = self._update_nu(s3, s4)
+                    else:
+                        norm1, norm2 = 1 / (i - polyak2 + 1), i - polyak2
+                        self.A = norm1 * (norm2 * self.A + self._update_A(v, S2, s3))
+                        self.nu = norm1 * (norm2 * self.nu + self._update_nu(s3, s4))
 
         self.pi_hist.append(copy.deepcopy(self.pi))
         self.mu_hist.append(copy.deepcopy(self.mu))
@@ -195,14 +268,19 @@ class GenStudentMixtures:
         self.D_hist.append(copy.deepcopy(self.D))
         self.nu_hist.append(copy.deepcopy(self.nu))
 
-        self.stat_hist.append([s0, s1, S2, s3, s4])
-
     def fit(self, X, gam, mini_batch=50):
         stat = {'s0': np.zeros(len(self.pi)),
                 's1': np.zeros(self.D.shape),
                 'S2': np.zeros((*self.D.shape, self.mu.shape[-1])),
                 's3': np.zeros(self.A.shape),
                 's4': np.zeros(self.A.shape)}
+        if self.burnin:
+            for i in range(5000):
+                y = X[i]
+                mst = MST(self.mu, self.A, self.D, self.nu).pdf(y)
+                r = self.pi * mst / MMST(self.pi).pdf(mst, y)
+                self.updateStat(y, r, gam[i // mini_batch], stat)
+
         for i in tqdm(range(0, len(X) - mini_batch, mini_batch)):
             stat_new = {'s0': np.zeros(len(self.pi)),
                         's1': np.zeros(self.D.shape),
@@ -221,10 +299,10 @@ class GenStudentMixtures:
                 stat_new['s4'] += stat_tmp['s4'] / mini_batch
 
             stat = copy.deepcopy(stat_new)
-            self._updateParams(stat)
+            self._updateParams(stat, i // mini_batch)
 
             if self.verbose:
-                if (i // mini_batch) % 500 == 0:
+                if (i // mini_batch) % 250 == 0:
                     print(self.pi)
 
             if self.max_iterations is not None:
